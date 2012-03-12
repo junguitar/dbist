545b2954f273ec5f5d30389859965ee225741ee6
junguitar@gmail.com <junguitar@gmail.com@4fe7dd7b-9452-32ff-6684-5cbe67923ba1>
2012-03-12 18:15:01 +0000

    dbist-example-webapp-1.0.4 release

---
+++ tags/dbist-1.0.4/dbist/src/main/java/org/dbist/dml/impl/DmlHibernate.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright 2011-2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dbist.dml.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.dbist.dml.AbstractDml;
+import org.dbist.dml.Dml;
+import org.dbist.dml.Page;
+import org.dbist.metadata.Table;
+
+/**
+ * @author Steve M. Jung
+ * @since 2011. 6. 2. (version 0.0.1)
+ */
+public class DmlHibernate extends AbstractDml implements Dml {
+
+	@Override
+	public Class<?> getClass(String tableName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Table getTable(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> void insert(T data) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void insertBatch(List<T> list) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void insert(T data, String... fieldNames) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void insertBatch(List<T> list, String... fieldNames) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void update(T data) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void updateBatch(List<T> list) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void update(T data, String... fieldNames) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void updateBatch(List<T> list, String... fieldNames) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> void delete(T data) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteBatch(List<?> list) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> int selectSize(Class<T> clazz, Object condition) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <T> List<T> selectList(Class<T> clazz, Object condition) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> List<T> selectListWithLock(Class<T> clazz, Object condition) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> List<T> selectListByQl(String ql, Map<String, ?> paramMap, Class<T> requiredType, int pageIndex, int pageSize) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> Page<T> selectPageByQl(String ql, Map<String, ?> paramMap, Class<T> requiredType, int pageIndex, int pageSize) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> List<T> selectListByQlPath(String qlPath, Map<String, ?> paramMap, Class<T> requiredType, int pageIndex, int pageSize)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> Page<T> selectPageByQlPath(String qlPath, Map<String, ?> paramMap, Class<T> requiredType, int pageIndex, int pageSize)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> int deleteList(Class<T> clazz, Object condition) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ tags/dbist-1.0.4/dbist/src/main/java/org/dbist/processor/impl/VelocityPreprocessor.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2009-2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dbist.processor.impl;
+
+import java.io.StringWriter;
+import java.util.Map;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.dbist.processor.Preprocessor;
+
+/**
+ * @author Steve M. Jung
+ * @since 2012. 2. 5. (version 0.0.1)
+ */
+public class VelocityPreprocessor implements Preprocessor {
+	VelocityEngine ve;
+
+	@Override
+	public String process(String value, Map<String, ?> contextMap) throws Exception {
+		if (ve == null) {
+			synchronized (this) {
+				if (ve == null) {
+					ve = new VelocityEngine();
+					ve.init();
+				}
+			}
+		}
+
+		StringWriter writer = new StringWriter();
+		VelocityContext context = new VelocityContext(contextMap);
+		ve.evaluate(context, writer, value, value);
+		return writer.toString();
+	}
+
+}

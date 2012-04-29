9d35fa8c2177540c4acd0e892b8225e3bf9bb268
junguitar <junguitar@4fe7dd7b-9452-32ff-6684-5cbe67923ba1>
2012-04-29 02:12:10 +0000

    dbist-example-webapp-1.0.9 release

---
+++ tags/dbist-example-webapp-1.0.9/dbist-example-webapp/src/main/java/org/dbist/example/blog/jpa/User.java
@@ -0,0 +1,52 @@
+package org.dbist.example.blog.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@Column(length = 100)
+	private String username;
+	@Column(name = "pwd", length = 100)
+	private String password;
+	@Column(name = "first_name", length = 100)
+	private String firstName;
+	@Column(name = "last_name", length = 100)
+	private String lastName;
+	@Column(length = 100)
+	private String email;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
